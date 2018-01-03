f75c1a5adaa6b135bed0f95fe368f60dc62e259a
Baurzhan <kz.baurzhan@gmail.com>
2018-01-03 18:22:13 +0600

    IO bug fixed

--- controllers/sellerController.js
+++ controllers/sellerController.js
@@ -2,7 +2,7 @@ const db = require('../lib/db');
 const httpStatus = require('http-status-codes');
 const invokeChaincode = require('./hl/invoke-transaction');
 const logger = require('../lib/logger');
-
+const config = require('config');
 /**
  *
  * @param req
@@ -16,19 +16,26 @@ const logger = require('../lib/logger');
  *
  */
 module.exports.advertise = (req, res) => {
-  const io = getIo();
-  const address = req.body.address;
-  const price = req.body.price;
-  const idnumber = req.body.idnumber;
+    // const io = getIo();
+    const address = req.body.address;
+    const price = req.body.price;
+    const idnumber = req.body.idnumber;
 
-  const owner = req.body.owner;
+    const owner = req.body.owner;
+    const body = {
+        hash: 'hash1',
+        address: address,
+        price: price,
+        idnumber: idnumber,
+        owner: owner,
+        txHash: '113213132132112321',
+        status: 'ADVERTISED'
+    };
+    if (!address || !price || !idnumber) {
+        return res.status(httpStatus.BAD_REQUEST).send({err: 'Invalid input paramteres'});
+    }
 
-  if (!address || !price || !idnumber) {
-    return res.status(httpStatus.BAD_REQUEST).
-        send({err: 'Invalid input paramteres'});
-  }
-
-  io.to('propertiesList room').emit('propertiesList', buyerController.fetchAssetsForSale());
+    // io.to('propertiesList room').emit('propertiesList', buyerController.fetchAssetsForSale());
 
     invokeChaincode.invokeChaincode('', config.get('channelName'), config.get('lending_chaincode'), 'advertise', [JSON.stringify(body)], 'admin', 'pocseller').then((response) => {
         return res.send(response);
