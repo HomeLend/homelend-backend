e8a8f55c06141c44e021097cf7dbd24b78803ead
ransht <ransht@gmail.com>
2018-01-09 12:55:49 +0200

    route fix

--- app.js
+++ app.js
@@ -54,6 +54,11 @@ app.use(API + 'seller', sellerRouter);
 app.use(API + 'auth', authRouter);
 app.use(API + 'buyer', buyerRouter);
 app.use(API + 'creditscore', creditScoreRouter);
+app.use(API + 'appraiser', appraiserRouter);
+app.use(API + 'government', governmentRouter);
+app.use(API + 'insurance', insuranceRouter);
+app.use(API + 'creditscore', creditScoreRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
