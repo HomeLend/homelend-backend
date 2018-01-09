a8c1afa167353dd78393a15ba33269fce4a88b9f
Baurzhan <kz.baurzhan@gmail.com>
2018-01-09 16:53:15 +0600

    calls fixed

--- routes/appraiserRouter.js
+++ routes/appraiserRouter.js
@@ -2,7 +2,5 @@ const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 const appraiserController = require('../controllers/appraiserController');
-
-router.get('/put-offer', appraiserController.putOffer);
-
+router.post('/put-offer', appraiserController.putOffer);
 module.exports = router;
