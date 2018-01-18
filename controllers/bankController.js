c9af7dc361c0e83ed94c2db112972f8821a82f0c
Gal Zakay <gal@nexil.co.il>
2018-01-18 11:25:54 +0200

    Converted bank controller to await, refactored some of the code

--- controllers/bankController.js
+++ controllers/bankController.js
@@ -1,31 +1,30 @@
 const db = require('../lib/db');
-const httpStatus = require('http-status-codes');
-const invokeChaincode = require('./hl/invoke-transaction');
+const { invokeChaincode }= require('./hl/invoke-transaction');
 const logger = require('../lib/logger');
 const queryChaincode = require('./hl/query');
-const Promise = require('bluebird');
 const config = require('config');
 const helper = require('./hl/helper');
 const UsersCacheModel = db.model('UsersCache');
 const chaincodeName = config.get('lending_chaincode');
 const org_name = 'org_pocbank';
 const uniqueString = require('unique-string');
-const { filter, last } = require('lodash');
+const {filter, last} = require('lodash');
 const attrs = [
-    {
-        'hf.Registrar.Roles': 'client,user,peer,validator,auditor',
-        'hf.Registrar.DelegateRoles': 'client,user,validator,auditor',
-        'hf.Revoker': true,
-        'hf.IntermediateCA': true,
-        //user role can be customized
-        BasicRole: 'admin',
-        'hf.Registrar.Attributes': '*',
-    }];
+	{
+		'hf.Registrar.Roles': 'client,user,peer,validator,auditor',
+		'hf.Registrar.DelegateRoles': 'client,user,validator,auditor',
+		'hf.Revoker': true,
+		'hf.IntermediateCA': true,
+		//user role can be customized
+		BasicRole: 'admin',
+		'hf.Registrar.Attributes': '*',
+	}];
 const dept = 'pocbank' + '.department1';
 const adminUsername = 'admin';
 const adminPassword = 'adminpw';
-module.exports.calculateRating = (req, res) => {
-	const { name, swiftNumber, userHash, requestHash, interest, monthlyPayment } = req.body;
+
+module.exports.calculateRating = async (req, res) => {
+	const {name, swiftNumber, userHash, requestHash, interest, monthlyPayment} = req.body;
 
 	const bankData = {
 		Name: name,
@@ -36,76 +35,69 @@ module.exports.calculateRating = (req, res) => {
 		UserHash: userHash
 	}
 
-	const interestFloat = parseFloat(interest)
-	const monthlyPaymentFloat = parseFloat(monthlyPayment)
-
-	const bankPutOfferData = [JSON.stringify(requestLink), uniqueString(), interestFloat + "", monthlyPaymentFloat + ""];
-	UsersCacheModel.findOne({ email: swiftNumber, type: 'bank' }).then((currentUser) => {
+	try {
+		const currentUser = await UsersCacheModel.findOne({email: swiftNumber, type: 'bank'})
+		let passOrSecret = get(currentUser, 'password');
 		if (!currentUser) {
-			return helper.register(org_name, swiftNumber, attrs, dept, adminUsername, adminPassword).then((registerResult) => {
-				if (!registerResult && !registerResult.secret) {
-					return res.status(400).send({ err: ' Problem registering agency' });
-				}
-				return UsersCacheModel({
-					email: swiftNumber,
-					password: registerResult.secret,
-					type: 'bank',
-					key: registerResult.key,
-					certificate: registerResult.certificate,
-					rootCertificate: registerResult.rootCertificate,
-				}).save().then((user) => {
-					if (!user) {
-						return res.status(400).send({ err: ' Problem saving the bank' });
-					}
-					return invokeChaincode.invokeChaincode(['peer0'], config.get('channelName'), chaincodeName, 'putBankInfo', [JSON.stringify(bankData)], org_name, swiftNumber, registerResult.secret).then((response) => {
-						if (!response) {
-							return res.status(400).send({ err: ' Problem saving the bank inside blockchain' });
-						}
-						return invokeChaincode.invokeChaincode(['peer0'], config.get('channelName'), chaincodeName, 'bankPutOffer', bankPutOfferData, org_name, swiftNumber, registerResult.secret).then((response) => {
-							if (!response) {
-								return res.status(400).send({ err: 'Problem updating bank offer' });
-							}
-							return res.status(200).send(response);
-						});
-					});
-				});
-			});
+			const registerResult = await helper.register(org_name, swiftNumber, attrs, dept, adminUsername, adminPassword)
+			if (!registerResult && !registerResult.secret) throw 'Problem registering agency';
+
+			const newBankDetails = {
+				email: swiftNumber,
+				password: registerResult.secret,
+				type: 'bank',
+				key: registerResult.key,
+				certificate: registerResult.certificate,
+				rootCertificate: registerResult.rootCertificate,
+			};
+			const user = await UsersCacheModel(newBankDetails).save();
+			if (!user) throw 'Problem saving the bank';
+	
+			const response = await invokeChaincode(['peer0'], config.get('channelName'), chaincodeName, 'putBankInfo', [JSON.stringify(bankData)], org_name, swiftNumber, registerResult.secret)
+			if (!response) throw 'Problem saving the bank inside blockchain';
+
+			passOrSecret = registerResult.secret;
 		}
-		else {
-			return invokeChaincode.invokeChaincode(['peer0'], config.get('channelName'), chaincodeName, 'bankPutOffer', bankPutOfferData, org_name, swiftNumber, currentUser.password).then((response) => {
-				if (!response) {
-					return res.status(400).send({ err: 'Problem updating bank offer' });
-				}
-				return res.status(200).send(response);
-			});
-		}
-	}).catch((err) => {
-		return res.status(400).send({ err: err });
-	});
+
+		const bankPutOfferData = [JSON.stringify(requestLink), uniqueString(), parseFloat(interest) + "", parseFloat(monthlyPayment) + ""];
+		const responseOffer = await invokeChaincode(['peer0'], config.get('channelName'), chaincodeName, 'bankPutOffer', bankPutOfferData, org_name, swiftNumber, passOrSecret)
+		if (!responseOffer) throw 'Problem updating bank offer';
+		
+		return res.status(200).send(responseOffer);
+	}
+	catch (err) {
+		console.log("Check bankController.js, calculateRating: ", err);
+		return res.status(400).send({err: err});
+	}
 };
 
 
-module.exports.pull = (req, res) => {
-	const { buyerHash } = req.body;
-	if(!buyerHash) throw 'No buyer hash received, make sure to provide a buyer hash before fetching requests'
+module.exports.pull = async (req, res) => {
+	const {buyerHash} = req.body;
+	try {
+		if (!buyerHash) throw 'No buyer hash received, make sure to provide a buyer hash before fetching requests'
 
-
-	return queryChaincode.queryChaincode(['peer0'], config.get('channelName'), chaincodeName, 'bankPullOpen4bankOffers', [JSON.stringify({})], org_name, 'admin', 'adminpw').then(async (response) => {
-    if (!response)
-      throw 'Not a proper response for bankPullOpen4bankOffers'
+		const response = await queryChaincode.queryChaincode(['peer0'], config.get('channelName'), chaincodeName, 'bankPullOpen4bankOffers', [JSON.stringify({})], org_name, 'admin', 'adminpw')
+		if (!response) throw 'Not a proper response for bankPullOpen4bankOffers'
 
 		let ret = response[0].toString('utf8');
-		if(ret) ret = JSON.parse(ret);
+		if (ret) ret = JSON.parse(ret);
 
 		ret = filter(ret, {UserHash: buyerHash});
-		ret = last(ret);
-		if(!ret) throw 'No recent requests'
+		if (!ret) throw 'No recent requests'
 
-    // Getting extended data about this request
+		// Get only the last request to present
+		ret = last(ret);
+
+		// Getting extended data about this request
 		let reqData = await queryChaincode.queryChaincode(['peer0'], config.get('channelName'), chaincodeName, 'getRequestInfo', [ret.UserHash, ret.RequestHash], org_name, 'admin', 'adminpw')
 		reqData = reqData[0].toString();
 		reqData = JSON.parse(reqData);
 
 		return res.status(200).send(reqData);
-  });
+	}
+	catch (err) {
+		console.log("Check bankController.js, pull: ", err);
+		return res.status(400).send({err: err});
+	}
 };
